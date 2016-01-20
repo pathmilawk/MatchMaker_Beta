e22a2532b835cbbd158f2efc05c0bd803868033a
pathmilawk <pathmilawk@gmail.com>
2016-01-20 13:39:43 +0530

    pathmila

---
+++ resources/views/client/index.blade.php
@@ -0,0 +1,31 @@
+@extends('master_page')
+@section('css_ref')
+    @parent
+@stop
+
+@section('content')
+
+    <h1>Kichiki</h1>
+    <h1>Kichiki</h1>
+    <h1>Kichiki</h1>
+    <h1>Kichiki</h1>
+    <h1>Kichiki</h1>
+    <h1>Kichiki</h1>
+    <h1>Kichiki</h1>
+    <h1>Kichiki</h1>
+    <h1>Kichiki</h1>
+    <h1>Kichiki</h1>
+    <h1>Kichiki</h1>
+    <h1>Kichiki</h1>
+    <h1>Kichiki</h1>
+    <h1>Kichiki</h1>
+
+
+
+@stop
+
+
+@section('js_ref')
+@parent
+
+@stop
