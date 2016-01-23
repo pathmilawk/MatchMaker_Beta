d64833306a69d054aa93843b2b099144dd763fa9
Damindu Liyanage <damindu7@gmail.com>
2016-01-23 12:43:36 +0000

    Revert "Register page integrated"
    This reverts commit 1e4d8147f0e68e21b80511b39e08f0aa845c81b4.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,38 +14,12 @@ class CreateUsersTable extends Migration {
 	{
 		Schema::create('users', function(Blueprint $table)
 		{
-			$table->bigIncrements('id');
-			$table->string('username');
-			$table->string('password', 60);
+			$table->increments('id');
+			$table->string('name');
 			$table->string('email')->unique();
-			$table->string('first_name');
-			$table->string('last_name');
-			$table->string('nickname');
-			$table->string('gender');
-			$table->string('interested_in');
-			$table->date('birthday');
-			$table->string('age');
-			$table->string('religion');
-			$table->string('ethnicity');
-			$table->string('country');
-			$table->string('hometown');
-			$table->string('location');
-			$table->string('address');
-			$table->string('telephone_mobile');
-			$table->string('telephone_home');
-			$table->string('telephone_office');
-			$table->text('education');
-			$table->text('work');
-			$table->string('languages');
-			$table->string('facebook');
-			$table->longText('about');
-			$table->boolean('is_verified');
-			$table->integer('cover_photo');
-			$table->integer('inbox');
-			$table->boolean('is_admin');
+			$table->string('password', 60);
 			$table->rememberToken();
 			$table->timestamps();
-
 		});
 	}
 
