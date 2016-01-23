fe29ad8c8354a0c0df3be19a9bb1fec18898bb9d
Damindu Liyanage <damindu7@gmail.com>
2016-01-23 08:37:37 +0530

    in route test route added

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,6 +15,8 @@ Route::get('/', 'WelcomeController@index');
 
 Route::get('home', 'HomeController@index');
 
+//Route::get('testDamindu', )
+
 Route::controllers([
 	'auth' => 'Auth\AuthController',
 	'password' => 'Auth\PasswordController',
