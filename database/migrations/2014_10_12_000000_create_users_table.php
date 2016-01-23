1e4d8147f0e68e21b80511b39e08f0aa845c81b4
Damindu Liyanage <damindu7@gmail.com>
2016-01-23 13:19:48 +0530

    Register page integrated

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,12 +14,38 @@ class CreateUsersTable extends Migration {
 	{
 		Schema::create('users', function(Blueprint $table)
 		{
-			$table->increments('id');
-			$table->string('name');
-			$table->string('email')->unique();
+			$table->bigIncrements('id');
+			$table->string('username');
 			$table->string('password', 60);
+			$table->string('email')->unique();
+			$table->string('first_name');
+			$table->string('last_name');
+			$table->string('nickname');
+			$table->string('gender');
+			$table->string('interested_in');
+			$table->date('birthday');
+			$table->string('age');
+			$table->string('religion');
+			$table->string('ethnicity');
+			$table->string('country');
+			$table->string('hometown');
+			$table->string('location');
+			$table->string('address');
+			$table->string('telephone_mobile');
+			$table->string('telephone_home');
+			$table->string('telephone_office');
+			$table->text('education');
+			$table->text('work');
+			$table->string('languages');
+			$table->string('facebook');
+			$table->longText('about');
+			$table->boolean('is_verified');
+			$table->integer('cover_photo');
+			$table->integer('inbox');
+			$table->boolean('is_admin');
 			$table->rememberToken();
 			$table->timestamps();
+
 		});
 	}
 
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,65 +1,146 @@
-@extends('app')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+	<meta name="description" content="">
+	<meta name="author" content="">
+	<!--<link rel="shortcut icon" href="../images/favicon.png" type="image/png">-->
 
-@section('content')
-<div class="container-fluid">
+	<title>Quirk Responsive Admin Templates</title>
+
+	<link rel="stylesheet" href="{{ asset('client/lib/fontawesome/css/font-awesome.css') }}">
+	<link rel="stylesheet" href="{{ asset('client/lib/select2/select2.css') }}">
+
+	<link rel="stylesheet" href="{{ asset('client/css/quirk.css') }}">
+
+	<script src="{{ asset('client/lib/modernizr/modernizr.js') }}"></script>
+	<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+	<!--[if lt IE 9]>
+	<script src="{{ asset('client/lib/html5shiv/html5shiv.js') }}"></script>
+	<script src="{{ asset('client/lib/respond/respond.src.js')}}"></script>
+	<![endif]-->
+</head>
+
+<body class="signwrapper">
+
+<div class="sign-overlay"></div>
+<div class="signpanel"></div>
+
+<div class="signup">
 	<div class="row">
-		<div class="col-md-8 col-md-offset-2">
-			<div class="panel panel-default">
-				<div class="panel-heading">Register</div>
+		<div class="col-sm-5">
+			<div class="panel">
+				<div class="panel-heading">
+					<h1>Quirk</h1>
+					<h4 class="panel-title">Create an Account!</h4>
+				</div>
 				<div class="panel-body">
-					@if (count($errors) > 0)
-						<div class="alert alert-danger">
-							<strong>Whoops!</strong> There were some problems with your input.<br><br>
-							<ul>
-								@foreach ($errors->all() as $error)
-									<li>{{ $error }}</li>
-								@endforeach
-							</ul>
+					<button class="btn btn-primary btn-quirk btn-fb btn-block">Sign Up Using Facebook</button>
+					<div class="or">or</div>
+					<form action="index.html">
+						<div class="form-group mb15">
+							<input type="text" class="form-control" placeholder="Enter Your Username">
 						</div>
-					@endif
+						<div class="form-group mb15">
+							<input type="text" class="form-control" placeholder="Enter Your Password">
+						</div>
+						<div class="form-group mb15">
+							<input type="text" class="form-control" placeholder="Enter Your Full Name">
+						</div>
 
-					<form class="form-horizontal" role="form" method="POST" action="/auth/register">
-						<input type="hidden" name="_token" value="{{ csrf_token() }}">
-
-						<div class="form-group">
-							<label class="col-md-4 control-label">Name</label>
-							<div class="col-md-6">
-								<input type="text" class="form-control" name="name" value="{{ old('name') }}">
+						<div class="row mb15">
+							<div class="col-xs-5">
+								<div class="form-group">
+									<select class="form-control" style="width: 100%" data-placeholder="Birth Month">
+										<option value="">&nbsp;</option>
+										<option value="January">January</option>
+										<option value="February">February</option>
+										<option value="March">March</option>
+										<option value="April">April</option>
+										<option value="May">May</option>
+										<option value="June">June</option>
+									</select>
+								</div>
+							</div>
+							<div class="col-xs-3">
+								<div class="form-group">
+									<select class="form-control" style="width: 100%" data-placeholder="Birth Day">
+										<option value="">&nbsp;</option>
+										<option value="01">01</option>
+										<option value="02">02</option>
+										<option value="03">03</option>
+										<option value="04">04</option>
+										<option value="05">05</option>
+										<option value="06">06</option>
+									</select>
+								</div>
+							</div>
+							<div class="col-xs-4">
+								<div class="form-group">
+									<select class="form-control" style="width: 100%" data-placeholder="Birth Year">
+										<option value="">&nbsp;</option>
+										<option value="1986">1986</option>
+										<option value="1987">1987</option>
+										<option value="1988">1988</option>
+									</select>
+								</div>
 							</div>
 						</div>
 
-						<div class="form-group">
-							<label class="col-md-4 control-label">E-Mail Address</label>
-							<div class="col-md-6">
-								<input type="email" class="form-control" name="email" value="{{ old('email') }}">
-							</div>
+						<div class="form-group mb20">
+							<label class="ckbox">
+								<input type="checkbox" name="checkbox">
+								<span>Accept terms and conditions</span>
+							</label>
 						</div>
-
 						<div class="form-group">
-							<label class="col-md-4 control-label">Password</label>
-							<div class="col-md-6">
-								<input type="password" class="form-control" name="password">
-							</div>
-						</div>
-
-						<div class="form-group">
-							<label class="col-md-4 control-label">Confirm Password</label>
-							<div class="col-md-6">
-								<input type="password" class="form-control" name="password_confirmation">
-							</div>
-						</div>
-
-						<div class="form-group">
-							<div class="col-md-6 col-md-offset-4">
-								<button type="submit" class="btn btn-primary">
-									Register
-								</button>
-							</div>
+							<button class="btn btn-success btn-quirk btn-block">Create Account</button>
 						</div>
 					</form>
+				</div><!-- panel-body -->
+			</div><!-- panel -->
+		</div><!-- col-sm-5 -->
+		<div class="col-sm-7">
+			<div class="sign-sidebar">
+				<h3 class="signtitle mb20">Two Good Reasons to Love Quirk</h3>
+				<p>When it comes to websites or apps, one of the first impression you consider is the design. It needs to be high quality enough otherwise you will lose potential users due to bad design.</p>
+				<p>Below are some of the reasons why you love Quirk.</p>
+
+				<br>
+
+				<h4 class="reason">1. Attractive</h4>
+				<p>When your website or app is attractive to use, your users will not simply be using it, they’ll look forward to using it. This means that you should fashion the look and feel of your interface for your users.</p>
+
+				<br>
+
+				<h4 class="reason">2. Responsive</h4>
+				<p>Responsive Web design is the approach that suggests that design and development should respond to the user’s behavior and environment based on screen size, platform and orientation. This would eliminate the need for a different design and development phase for each new gadget on the market.</p>
+
+				<hr class="invisible">
+
+				<div class="form-group">
+					<a href="login" class="btn btn-default btn-quirk btn-stroke btn-stroke-thin btn-block btn-sign">Already a member? Sign In Now!</a>
 				</div>
-			</div>
+			</div><!-- sign-sidebar -->
 		</div>
 	</div>
-</div>
-@endsection
+</div><!-- signup -->
+
+
+
+<script src="{{ asset('client/lib/jquery/jquery.js') }}"></script>
+<script src="{{ asset('client/lib/bootstrap/js/bootstrap.js') }}"></script>
+<script src="{{ asset('client/lib/select2/select2.js') }}"></script>
+
+<script>
+	$(function() {
+
+		// Select2 Box
+		$("select.form-control").select2({ minimumResultsForSearch: Infinity });
+
+	});
+</script>
+
+</body>
+</html>
