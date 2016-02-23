cf89cb5824e31b310fd96ee21819dd1a896b29e8
Winfast <prageethkalhara17@yahoo.com>
2016-02-24 00:17:39 +0530

    First Commit

--- resources/views/Mingle/mingleView.blade.php
+++ resources/views/Mingle/mingleView.blade.php
@@ -1,4 +1,11 @@
 
+@extends('master_page')
+@section('css_ref')
+    @parent
+@stop
+
+@section('content')
+
 <?php foreach($result as $row){
 
 if (Session::has('val'))
@@ -27,7 +34,16 @@ else{
 <img  src="{{asset($row->image_path)}}" alt="" />
 <form action="getnext<?php echo $x;$x++;  if($x<9){Session::put('val',$x);} else{$value = Session::pull('val', 1);
 }?>" method="post">
-    <input type="submit" name="next" value="Next">
+    <input type="submit" name="next" value="Next" class="">
 
 </form>
 <?php $x++; }?>
+
+
+@stop
+
+
+@section('js_ref')
+    @parent
+
+@stop
