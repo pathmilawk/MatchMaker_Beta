bf7db0de7bcee5eb5f5e03a3451a5e21e16d9911
SavidyaSathyani <savidya19@gmail.com>
2016-04-07 10:36:11 +0530

    notification + friend profile

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -1,27 +1,18 @@
 <?php namespace App\Http\Controllers;
 
 use App\Http\Requests;
-use App\Http\Controllers\Controller;
-use App\Http\Requests\ContactRequest;
-use App\Story;
-use Carbon\Carbon;
-use App\Http\Requests\StoryRequest;
-use App\Http\Requests\CommentRequest;
 use DB;
 use Illuminate\Http\Request;
 use Image;
 use Session;
-use App\Comment;
-use App\Notification;
-use App\Http\Requests\NotificationRequest;
 
 class NotificationController extends Controller {
 
     public function refreshNotifications(Request $request){
 
-        $receiver_username = $request->Input('receiver_username');
-        $notificationCount = DB::table('notifications')->where('receiver_username', 'Savidya')->count();
-        $results=DB::table('notifications')->where('receiver_username', 'Savidya')->get();
+        $receiver_username = Auth:: user()->name;
+        $notificationCount = DB::table('notifications')->where('receiver_username', $receiver_username)->count();
+        $results=DB::table('notifications')->where('receiver_username', $receiver_username)->get();
         return view('ajax.notification')->with('notificationCount',$notificationCount)->with('results',$results);
     }
 
